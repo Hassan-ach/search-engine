f4c4aa638cd11ec4d31b57b06668d1f47c952d4b
bagi <hassanachlou@gmail.com>
2026-02-27 21:52:13 +0000

    add two diff enty point of the app {server(web), cli(terminal)}

---
+++ services/query-engine/cmd/cli/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+	"time"
+
+	"query-engine/internal/config"
+	"query-engine/internal/service"
+	"query-engine/internal/store"
+)
+
+type mockSpeller struct{}
+
+func (s mockSpeller) GetSuggestions(q string) []string {
+	return strings.Split(q, " ")
+}
+
+func main() {
+	start := time.Now()
+	if len(os.Args) < 2 {
+		fmt.Println("missing input")
+		os.Exit(1)
+	}
+	conf, err := config.LoadConfig("../../.env")
+	store := store.NewStore(conf.Store)
+
+	// speller, err := service.NewAspellSpellingService()
+	// if err != nil {
+	// 	panic("failed to initialize speller: " + err.Error())
+	// }
+	speller := mockSpeller{}
+
+	ranker := service.NewRankingService(&store, conf.Ranker)
+
+	query := speller.GetSuggestions(os.Args[1])
+	fmt.Printf("query: %s\n", query)
+
+	s := time.Now()
+	pages, err := ranker.Rank(query)
+	fmt.Printf("ranking took: %d ms\n", time.Since(s).Abs().Milliseconds())
+	if err != nil {
+		fmt.Printf("err: %v", err)
+	}
+
+	for _, page := range pages {
+		fmt.Printf("%s: %f  Keywords: %v\n ", page.URL, page.GlobalScore, page.MetaData.Keywords)
+	}
+	fmt.Printf("total execution time: %d ms\n", time.Since(start).Abs().Milliseconds())
+}
---
+++ services/query-engine/cmd/server/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+
+	"query-engine/internal/config"
+	"query-engine/internal/handlers"
+	"query-engine/internal/service"
+	"query-engine/internal/store"
+
+	"github.com/labstack/echo/v5"
+	"github.com/labstack/echo/v5/middleware"
+)
+
+func main() {
+	conf, err := config.LoadConfig("../../.env")
+	if err != nil {
+		panic("failed to load config: " + err.Error())
+	}
+
+	e := echo.New()
+	e.Use(middleware.RequestLogger())
+
+	store := store.NewStore(conf.Store)
+
+	speller, err := service.NewAspellSpellingService()
+	if err != nil {
+		panic("failed to initialize speller: " + err.Error())
+	}
+
+	ranker := service.NewRankingService(&store, conf.Ranker)
+
+	homeHandler := &handlers.HomeHandler{}
+	rankingHandler := handlers.NewSearchHandler(ranker, speller)
+
+	fmt.Printf("starting server on port 1323\n")
+
+	e.GET("/", homeHandler.Handle)
+	e.GET("/search", rankingHandler.Handle)
+
+	if err := e.Start(":1323"); err != nil {
+		e.Logger.Error("failed to start server", "error", err)
+	}
+}
